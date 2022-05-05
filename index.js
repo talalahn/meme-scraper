80c8ab947964c33c94a2be9bb34a5615e6ffc83c
Tal Alon <tal_alon@msn.com>
2022-05-05 16:38:12 +0200

    Add stretch goal 1 code

--- index.js
+++ index.js
@@ -10,43 +10,64 @@ if (!fs.existsSync(dir)) {
   // check if folder already exists
   fs.mkdirSync(dir); // creating folder
 }
-request(URL_TO_PARSE, (err, response, body) => {
-  if (err) return console.error(err);
+const dir2 = './custom_meme';
 
-  const $ = cheerio.load(body);
+if (!fs.existsSync(dir2)) {
+  // check if folder already exists
+  fs.mkdirSync(dir2); // creating folder
+}
+if (!process.argv[2]) {
+  request(URL_TO_PARSE, (err, response, body) => {
+    if (err) return console.error(err);
 
-  const allImages = [];
+    const $ = cheerio.load(body);
 
-  $('img').each(function (i) {
-    allImages[i] = $(this).attr('src');
+    const allImages = [];
+
+    $('img').each(function (i) {
+      allImages[i] = $(this).attr('src');
+    });
+
+    const imagesWithJpgEnding = allImages.map((image) => {
+      return image.slice(0, -10);
+    });
+    const download = function (url, filename, callback) {
+      request.head(url, function () {
+        request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
+      });
+    };
+
+    for (let counter = 0; counter < 10; counter++) {
+      if (counter < 9) {
+        download(
+          imagesWithJpgEnding[counter],
+          `./memes/0${counter + 1}.jpg`,
+          function () {
+            console.log('Image downloaded');
+          },
+        );
+      } else {
+        download(
+          imagesWithJpgEnding[counter],
+          `./memes/${counter + 1}.jpg`,
+          function () {
+            console.log('Image downloaded');
+          },
+        );
+      }
+    }
   });
-
-  const imagesWithJpgEnding = allImages.map((image) => {
-    return image.slice(0, -10);
-  });
+} else if (process.argv[3]) {
   const download = function (url, filename, callback) {
     request.head(url, function () {
       request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   };
-
-  for (let counter = 0; counter < 10; counter++) {
-    if (counter < 9) {
-      download(
-        imagesWithJpgEnding[counter],
-        `./memes/0${counter + 1}.jpg`,
-        function () {
-          console.log('Image downloaded');
-        },
-      );
-    } else {
-      download(
-        imagesWithJpgEnding[counter],
-        `./memes/${counter + 1}.jpg`,
-        function () {
-          console.log('Image downloaded');
-        },
-      );
-    }
-  }
-});
+  download(
+    `https://api.memegen.link/images/${process.argv[4]}/${process.argv[2]}/${process.argv[3]}.png`,
+    `./custom_meme/custom.png`,
+    function () {
+      console.log('Custom image downloaded');
+    },
+  );
+}
