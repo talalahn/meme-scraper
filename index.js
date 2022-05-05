309dcb1e42c01e7c993566985c467075ade8af31
Tal Alon <tal_alon@msn.com>
2022-05-05 15:05:05 +0200

    Add code to make program work

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+import fs, { writeFile } from 'node:fs';
 import cheerio from 'cheerio';
 import request from 'request';
 
 const URL_TO_PARSE = 'https://memegen-link-examples-upleveled.netlify.app/';
 
+let dir = './memes'; //name of the directory/folder
+
+if (!fs.existsSync(dir)) {
+  //check if folder already exists
+  fs.mkdirSync(dir); //creating folder
+}
 request(URL_TO_PARSE, (err, response, body) => {
   if (err) return console.error(err);
 
@@ -15,37 +22,24 @@ request(URL_TO_PARSE, (err, response, body) => {
   });
   // console.log(allImages);
   const firstTenImages = allImages.slice(0, 10);
-  console.log(firstTenImages);
+
+  const realFirstTenImages = firstTenImages.map((image) => {
+    return image.slice(0, -10);
+  });
+
+  const download = function (url, filename, callback) {
+    request.head(url, function (err, res, body) {
+      request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+  };
+
+  for (let counter = 0; counter < 10; counter++) {
+    download(
+      realFirstTenImages[counter],
+      `./memes/0${counter + 1}.jpg`,
+      function () {
+        console.log('Image downloaded');
+      },
+    );
+  }
 });
-
-//   console.log($('img').attr('src'));
-// });
-
-//   console.log(images);
-// });
-// import { isString } from 'util';
-// import axios from 'axios';
-// request(url, function (error, response, body) {
-//   console.error('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   let splitHtml = ('body:', body).split('/n'); // Print the HTML for the url.
-//   // console.log(splitHtml.match('jpg'));
-//   console.log(splitHtml);
-//   return splitHtml.toString;
-// });
-
-// import fetch from 'node-fetch';
-
-// const response = await fetch(
-//   'https://memegen-link-examples-upleveled.netlify.app/',
-// );
-// const body = await response.text();
-// const htmlString = body.split('/n');
-// // console.log(htmlString)
-
-// const htmlString = async () => {
-//   const response = await axios.get(
-//     'https://memegen-link-examples-upleveled.netlify.app/',
-//   );
-//   console.log(response);
-// };
