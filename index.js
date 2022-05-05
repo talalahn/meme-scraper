32cdd57fa8198cf38b64c0762e1d737d1d249178
Tal Alon <tal_alon@msn.com>
2022-05-05 15:38:34 +0200

    Edit code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import fs, { writeFile } from 'node:fs';
+import fs from 'node:fs';
 import cheerio from 'cheerio';
 import request from 'request';
 
 const URL_TO_PARSE = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-let dir = './memes'; //name of the directory/folder
+const dir = './memes';
 
 if (!fs.existsSync(dir)) {
-  //check if folder already exists
-  fs.mkdirSync(dir); //creating folder
+  // check if folder already exists
+  fs.mkdirSync(dir); // creating folder
 }
 request(URL_TO_PARSE, (err, response, body) => {
   if (err) return console.error(err);
@@ -17,29 +17,36 @@ request(URL_TO_PARSE, (err, response, body) => {
 
   const allImages = [];
 
-  $('img').each(function (i, e) {
+  $('img').each(function (i) {
     allImages[i] = $(this).attr('src');
   });
-  // console.log(allImages);
-  const firstTenImages = allImages.slice(0, 10);
 
-  const realFirstTenImages = firstTenImages.map((image) => {
+  const imagesWithJpgEnding = allImages.map((image) => {
     return image.slice(0, -10);
   });
-
   const download = function (url, filename, callback) {
-    request.head(url, function (err, res, body) {
+    request.head(url, function () {
       request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   };
 
   for (let counter = 0; counter < 10; counter++) {
-    download(
-      realFirstTenImages[counter],
-      `./memes/0${counter + 1}.jpg`,
-      function () {
-        console.log('Image downloaded');
-      },
-    );
+    if (counter < 9) {
+      download(
+        imagesWithJpgEnding[counter],
+        `./memes/0${counter + 1}.jpg`,
+        function () {
+          console.log('Image downloaded');
+        },
+      );
+    } else {
+      download(
+        imagesWithJpgEnding[counter],
+        `./memes/${counter + 1}.jpg`,
+        function () {
+          console.log('Image downloaded');
+        },
+      );
+    }
   }
 });
