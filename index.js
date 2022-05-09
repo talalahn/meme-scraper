dd1bca949a360c26427a7572d3594a76d4aad5eb
Tal Alon <tal_alon@msn.com>
2022-05-09 09:27:56 +0200

    Add .catch statement

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-import { doesNotMatch } from 'node:assert';
-import { checkPrime } from 'node:crypto';
 import fs from 'node:fs';
-import { TransformStreamDefaultController } from 'node:stream/web';
 import cheerio from 'cheerio';
-import cliProgress from 'cli-progress';
 import request from 'request';
 
 const memeSiteUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
@@ -14,15 +10,7 @@ const customMemeFolder = './custom_meme';
 const customMemeUrl = `https://api.memegen.link/images/${process.argv[4]}/${process.argv[2]}/${process.argv[3]}.png`;
 
 const imageDownloaded = `Image downloaded`;
-if (!fs.existsSync(memeFolder)) {
-  // check if folder already exists
-  fs.mkdirSync(memeFolder); // creating folder
-}
 
-if (!fs.existsSync(customMemeFolder)) {
-  // check if folder already exists
-  fs.mkdirSync(customMemeFolder); // creating folder
-}
 if (!process.argv[2]) {
   request(memeSiteUrl, (err, response, body) => {
     if (err) return console.error(err);
@@ -30,7 +18,10 @@ if (!process.argv[2]) {
     const $ = cheerio.load(body);
 
     const allImages = [];
-
+    if (!fs.existsSync(memeFolder)) {
+      // check if folder already exists
+      fs.mkdirSync(memeFolder); // creating folder
+    }
     $('img').each(function (i) {
       allImages[i] = $(this).attr('src');
     });
@@ -48,16 +39,14 @@ if (!process.argv[2]) {
       async function progressBar() {
         for (let i = 0; i < 10; i++) {
           const dots = '.'.repeat(100);
-          const left = 9 - i;
-          const empty = ' '.repeat(left);
+          // const left = 9 - i;
+          // const empty = ' '.repeat(left);
           process.stdout.write(`\r[${dots}] ${(i + 1) * 10}%`);
-          // process.stdout.write(
-          //   `\r${dots.replaceAt([i + 1], 'x')} ${(i + 1) * 10}%`,
 
           await imageDownloaded;
         }
       }
-      progressBar();
+      progressBar().catch((error) => console.log(error));
       if (counter < 9) {
         download(
           imagesWithJpgEnding[counter],
@@ -85,6 +74,10 @@ if (!process.argv[2]) {
       request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   };
+  if (!fs.existsSync(customMemeFolder)) {
+    // check if folder already exists
+    fs.mkdirSync(customMemeFolder);
+  } // creating folder
   download(customMemeUrl, `./custom_meme/custom.png`, function () {
     console.log('Custom Image Downloaded');
   });
